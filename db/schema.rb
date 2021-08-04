d8005c653c95eee736cf91d0cb6f8486976ffb22
Aiden Isaia <aidenisaia@gmail.com>
2021-08-03 19:25:10 -0700

    merge files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_004749) do
+ActiveRecord::Schema.define(version: 2021_08_04_005103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,9 +25,48 @@ ActiveRecord::Schema.define(version: 2021_08_04_004749) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "educations", force: :cascade do |t|
+    t.integer "student_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "degree"
+    t.string "university_name"
+    t.string "details"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.integer "student_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "job_title"
+    t.string "company_name"
+    t.string "details"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "skills", force: :cascade do |t|
     t.integer "student_id"
     t.string "skill_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "phone_number"
+    t.string "short_bio"
+    t.string "linkedin_url"
+    t.string "twitter_handle"
+    t.string "website_url"
+    t.string "online_resume_url"
+    t.string "github_url"
+    t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
