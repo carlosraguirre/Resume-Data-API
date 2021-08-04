19a097450c7af74791669c6b329d4fe624b4014a
Adrian <aguerra_57@yahoo.com>
2021-08-03 20:50:23 -0500

    merge file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   get '/experience' => 'experience#index'
-=======
   #show all groups
   get "/education" => "education#index"
   #show one specific group
@@ -13,5 +11,4 @@ Rails.application.routes.draw do
   patch "/education/:id" => "education#update"
   #delete existing group
   delete "/education/:id" => "education#destroy"
->>>>>>> 980b03d3ae658149c056f0735f6431930063637d
 end
