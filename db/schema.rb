91494e416b1c7802abd8f4acb08e06ac4fb74d1d
Taegon Kim <taegon_kim@brown.edu>
2021-08-05 10:24:50 +0900

    seeds file data addition

--- db/schema.rb
+++ db/schema.rb
@@ -70,5 +70,4 @@ ActiveRecord::Schema.define(version: 2021_08_04_005103) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
